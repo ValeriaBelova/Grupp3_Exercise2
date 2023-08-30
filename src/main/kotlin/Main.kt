428323ac45aeb92f4d7c093fbf8d9ae697724759
ThunsOJ <oscar_jidaker@icloud.com>
2023-08-30 14:29:46 +0200

    Added brute force algo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,36 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.system.measureTimeMillis
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val hiddenWord = "o*s03"
+    val characterSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+
+    var found = false
+    var length = 2  // Start with length 1
+
+    val elapsedTime = measureTimeMillis {
+        while (!found) {
+            val current = CharArray(length)
+            found = generateCombinations(characterSet.toCharArray(), current, hiddenWord, 0)
+            if (found) {
+                println("Cracked: ${String(current)}")
+            }
+            length++
+        }
+    }
+
+    println("Elapsed time: $elapsedTime ms")
+}
+
+fun generateCombinations(set: CharArray, current: CharArray, hiddenWord: String, position: Int): Boolean {
+    if (position == current.size) {
+        return current.contentEquals(hiddenWord.toCharArray())
+    }
+
+    for (char in set) {
+        current[position] = char
+        if (generateCombinations(set, current, hiddenWord, position + 1)) {
+            return true
+        }
+    }
+    return false
 }
